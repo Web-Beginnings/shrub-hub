d7df264bc8048ef86d56da62c54a99afc0347605
Zak Saleh <zaksaleh@sky.com>
2023-04-28 13:56:41 +0100

    new-chage-test-2

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi there</Text>
+      <Text>SHRUB HUB UNITE!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
