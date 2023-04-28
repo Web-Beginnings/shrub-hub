5f31fd0c042f9661485b5005675276130f13070f
Zak Saleh <zaksaleh@sky.com>
2023-04-28 15:55:25 +0100

    firebase-initialisation

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACnNgUBeXsUuqVjw61yDjv9jqnfRFrQUw",
+  authDomain: "shrub-hub.firebaseapp.com",
+  projectId: "shrub-hub",
+  storageBucket: "shrub-hub.appspot.com",
+  messagingSenderId: "890051548377",
+  appId: "1:890051548377:web:0ac68cb8c596d8890b5ac8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
