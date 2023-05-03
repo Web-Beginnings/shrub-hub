45159dd740807d4785d9e48aa385dc2cd8e84f51
Matthew Treloar <matthewtreloar@macbook-pro.home>
2023-05-03 09:01:00 +0100

    plant list page

--- src/screens/PlantsList.js
+++ src/screens/PlantsList.js
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, TouchableOpacity } from "react-native";
 import Footer from "./HomeScreen/Components.js/Footer";
 import Header from "./HomeScreen/Components.js/Header";
 import styles from "./HomeScreen/styles";
 import { getPlants } from "./Api";
-
+import { ScrollView } from "react-native-gesture-handler";
 export default function PlantsList(props, extraData) {
   const [plantsArray, setPlantsArray] = useState([]);
-
   useEffect(() => {
     getPlants().then((result) => {
       setPlantsArray(result);
     });
   }, [setPlantsArray]);
-
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {plantsArray.map((plant) => {
-          console.log(plant, "plant");
-          return <Text key={plant.id}>{plant.common_name}</Text>;
-        })}
+        <ScrollView>
+          {plantsArray.map((plant) => {
+            // let plantName = plant.scientific_name;
+            return (
+              <TouchableOpacity key={plant.id}>
+                <Text>
+                  {plant.scientific_name} AKA: {plant.common_name}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
       </View>
       <View style={styles.footer}>
         <Footer />
