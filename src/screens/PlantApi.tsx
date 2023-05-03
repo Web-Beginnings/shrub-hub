9abab41541c69510806cfab89e8b8542a69548ba
Daniel-J-94 <danieljowett@outlook.com>
2023-05-03 18:12:12 +0100

    formatting

--- src/screens/PlantApi.tsx
+++ src/screens/PlantApi.tsx
@@ -24,12 +24,12 @@ default_image: PlantDefaultImage
 interface PlantDefaultImage {
     license: number,
     license_name: string,
-license_url: string,
-original_url: string,
-regular_url: string,
-medium_url: string,
-small_url: string,
-thumbnail: string
+    license_url: string,
+    original_url: string,
+    regular_url: string,
+    medium_url: string,
+    small_url: string,
+    thumbnail: string
 }
 
 export async function getPlants (): Promise<PlantData[]> {
