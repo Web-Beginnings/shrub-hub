57f68ded2e5bcbc564f1156d30f15efa9e49de98
Matthew Treloar <matthewtreloar@macbook-pro.home>
2023-05-05 16:40:59 +0100

    adding to my plants

--- src/screens/PlantCardScreen/PlantCard.tsx
+++ src/screens/PlantCardScreen/PlantCard.tsx
@@ -10,6 +10,18 @@ import {
 } from "react-native";
 import Footer from "../HomeScreen/Components.js/Footer";
 import { useNavigation } from "@react-navigation/native";
+import { firebase } from "../../../firebaseConfig";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  doc,
+  onSnapshot,
+  where,
+  setDoc,
+} from "firebase/firestore";
 
 interface PlantCardProps {
   route: any;
@@ -20,14 +32,65 @@ export default function PlantCard(props: PlantCardProps) {
   const { route } = props;
   const navigation = useNavigation();
   const id = route.params.plantId;
+
   useEffect(() => {
     getPlantsById(id).then((result) => {
       setPlant(result);
     });
   }, [id]);
+
   if (!plant) {
     return <Text>Loading Plant...</Text>;
   }
+
+  const db = getFirestore();
+
+  // This code below gets the data from myPlants for the user - This will be needed for My Plants page.
+  // const colRef = collection(
+  //   db,
+  //   "users",
+  //   "GZ0fx9ooAAWqHxPpKynjFRmq3cj2",
+  //   "myPlants"
+  // );
+
+  // const user = firebase.auth().currentUser;
+  // const users: any = [];
+
+  // getDocs(colRef)
+  //   .then((snapshot) => {
+  //     snapshot.docs.filter((doc) => {
+  //       users.push({ ...doc.data(), id: doc.id });
+  //     });
+  //     console.log("users", users);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  const postPlant = (id: number) => {
+    const user = firebase.auth().currentUser;
+    // init services
+    const db = getFirestore();
+    if (!user) {
+      console.log("No user found");
+      return;
+    }
+
+    const { uid } = user;
+
+    const dbRef = collection(db, "users", `${uid}`, "myPlants");
+
+    const data = {
+      plant_id: id,
+    };
+    addDoc(dbRef, data)
+      .then((docRef) => {
+        console.log("Document has been added successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <View style={styles.content}>
@@ -38,7 +101,9 @@ export default function PlantCard(props: PlantCardProps) {
           style={styles.plantImage}
         />
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>+ My Plants</Text>
+          <Text style={styles.buttonText} onPress={() => postPlant(id)}>
+            + My Plants
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>+ Wish List</Text>
@@ -79,9 +144,9 @@ export default function PlantCard(props: PlantCardProps) {
           <Text style={styles.plantInfo}>
             <Text style={styles.plantTitles}>Flowering Season: </Text>
             {plant.flowering_season}
-          </Text >
+          </Text>
           <Text style={styles.plantInfo}>
-            <Text style={styles.plantTitles}>Propagation: </Text >
+            <Text style={styles.plantTitles}>Propagation: </Text>
             {plant.propagation}
           </Text>
           <Text>
@@ -101,17 +166,17 @@ const styles = StyleSheet.create({
   content: {
     paddingHorizontal: 20,
     paddingBottom: 10,
-    backgroundColor: "#484240"
+    backgroundColor: "#484240",
   },
   info: {
-    paddingVertical: 20
+    paddingVertical: 20,
   },
   plantTitles: {
     fontWeight: "bold",
-    color: "#EA9547"
+    color: "#EA9547",
   },
   plantInfo: {
-    color: "white"
+    color: "white",
   },
   plantImage: {
     width: "75%",
@@ -120,7 +185,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 20,
     alignSelf: "center",
-    borderRadius: 100
+    borderRadius: 100,
   },
   title: {
     paddingTop: 10,
@@ -128,7 +193,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
-    color: "#EA9547"
+    color: "#EA9547",
   },
   button: {
     backgroundColor: "#EA9547",
