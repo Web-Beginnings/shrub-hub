43de7d2bce3e7a0017284345a3079a124432bcf4
Daniel-J-94 <danieljowett@outlook.com>
2023-05-05 16:17:13 +0100

    forumworking

--- src/screens/SinglePost.js
+++ src/screens/SinglePost.js
@@ -1,39 +1,39 @@
-import { Text, View, StyleSheet } from 'react-native';
-import Footer from './HomeScreen/Components.js/Footer';
+import { Text, View, StyleSheet } from "react-native";
+import Footer from "./HomeScreen/Components.js/Footer";
 
 const SinglePost = (props) => {
+  const navigation = props.navigation;
+  const post = props.route.params.props;
 
-    const post = props.route.params.props;
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.content}>
-                <Text style={styles.title}>{post.title}</Text>
-                <Text style={styles.body}>{post.body}</Text>
-            </View>
-        <Footer style={styles.footer}/>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>{post.title}</Text>
+        <Text style={styles.body}>{post.body}</Text>
+      </View>
+      <Footer style={styles.footer} navigation={navigation} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'space-between',
-    },
-    content: {
-        flex: 1,
-    },
-    title: {
-        fontSize: 20,
-    },
-    body: {
-        fontSize: 15,
-    },
-    footer: {
-        padding: 25,
-        backgroundColor: "blue",
-      },
-})
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  content: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+  },
+  body: {
+    fontSize: 15,
+  },
+  footer: {
+    padding: 25,
+    backgroundColor: "blue",
+  },
+});
 
 export default SinglePost;
