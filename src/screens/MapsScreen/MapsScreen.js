7362fea6a1199187804a1896d102311017f70c05
Zak Saleh <zaksaleh@sky.com>
2023-05-09 09:00:48 +0100

    maps-file-seperation

--- src/screens/MapsScreen/MapsScreen.js
+++ src/screens/MapsScreen/MapsScreen.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
 import { Image } from "react-native";
-import MapView, {
-  Callout,
-  Marker,
-  PROVIDER_GOOGLE,
-  Animated,
-  AnimatedRegion,
-} from "react-native-maps";
+import { cityCoordinates, cityOptions } from "./CityCoordinates";
+import styles from "./styles";
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import {
   Text,
   View,
@@ -23,8 +19,6 @@ import {
 import SelectDropdown from "react-native-select-dropdown";
 
 import { GOOGLE_API_KEY } from "../../../environment";
-import * as Location from "expo-location";
-
 import Footer from "../HomeScreen/Components.js/Footer";
 
 export default function MapsScreen({ navigation }) {
@@ -37,54 +31,6 @@ export default function MapsScreen({ navigation }) {
   const [plantShops, setPlantShops] = useState([]);
   const [search, setSearch] = useState("United Kingdom");
   const [buttonPress, setButtonPress] = useState(false);
-
-  const cityOptions = [
-    "Leeds",
-    "Manchester",
-    "York",
-    "London",
-    "Sheffield",
-    "Birmingham",
-  ];
-
-  const cityCoordinates = {
-    London: {
-      latitude: 51.497479011894086,
-      latitudeDelta: 0.1740534870343282,
-      longitude: -0.11505637317895889,
-      longitudeDelta: 0.1791045069694519,
-    },
-    York: {
-      latitude: 53.97662994598687,
-      latitudeDelta: 0.18917522098996642,
-      longitude: -1.0799391567707062,
-      longitudeDelta: 0.2060627192258836,
-    },
-    Sheffield: {
-      latitude: 53.364104927032244,
-      latitudeDelta: 0.18848379442276553,
-      longitude: -1.4647399261593819,
-      longitudeDelta: 0.2023465186357496,
-    },
-    Leeds: {
-      latitude: 53.80130280280241,
-      latitudeDelta: 0.2503246670873196,
-      longitude: -1.5464437007904053,
-      longitudeDelta: 0.27153007686138153,
-    },
-    Birmingham: {
-      latitude: 52.491348016535234,
-      latitudeDelta: 0.2178187321831615,
-      longitude: -1.8559132888913155,
-      longitudeDelta: 0.2291712909936907,
-    },
-    Manchester: {
-      latitude: 53.47313110392403,
-      latitudeDelta: 0.2075178147392478,
-      longitude: -2.2210343554615974,
-      longitudeDelta: 0.22335223853588104,
-    },
-  };
 
   const animate = (selectedItem) => {
     let chosenCity = {
@@ -108,7 +54,7 @@ export default function MapsScreen({ navigation }) {
   }, [search]);
 
   const onRegionChange = (region) => {
-    console.log(region);
+    // console.log(region);
   };
 
   const handleOnSelect = (selectedItem) => {
@@ -153,7 +99,6 @@ export default function MapsScreen({ navigation }) {
             style={styles.Icon}
             source={require("../../../assets/FindThosePlantsIcon.png")}
           />
-          {/* <Text style={styles.header}>FIND THOSE PLANTS</Text> */}
         </View>
 
         <View style={styles.spacer}>
@@ -203,117 +148,6 @@ export default function MapsScreen({ navigation }) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  spacer: {
-    padding: 10,
-    color: "#EA9547",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#484240",
-    paddingTop: "17%",
-  },
-  bubble: {
-    flexDirection: "column",
-    alignSelf: "flex-start",
-    backgroundColor: "#fff",
-    borderRadius: 6,
-    borderColor: "#ccc",
-    borderWidth: 0.5,
-    padding: 15,
-    width: 150,
-  },
-  name: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  input: {
-    height: 80,
-    borderRadius: 5,
-    overflow: "hidden",
-    backgroundColor: "white",
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 30,
-    marginRight: 30,
-    paddingLeft: 16,
-  },
-  arrow: {
-    backgroundColor: "transparent",
-    borderColor: "transparent",
-    borderTopColor: "#fff",
-    borderWidth: 16,
-    alignSelf: "center",
-    marginTop: -32,
-  },
-  arrowBorder: {
-    backgroundColor: "transparent",
-    borderColor: "transparent",
-    borderTopColor: "#007a87",
-    borderWidth: 16,
-    alignSelf: "center",
-    marginTop: -0.5,
-  },
-  body: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 10,
-  },
-  map: {
-    width: "90%",
-    height: "60%",
-    borderColor: "#3A914F",
-    paddingVertical: 30,
-    marginBottom: 50,
-    borderRadius: 30,
-  },
-  footer: {
-    backgroundColor: "#484240",
-    paddingBottom: 10,
-    paddingTop: 10,
-    paddingHorizontal: 25,
-  },
-  header: {
-    fontSize: 20,
-    fontWeight: "700",
-  },
-  searchContainer: {
-    position: "absolute",
-    height: "5%",
-    width: "65%",
-    backgroundColor: "white",
-    shadowColor: "black",
-    shadowOffset: { width: 2, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 4,
-    elevation: 4,
-    padding: 8,
-    borderRadius: 8,
-  },
-  input: {
-    borderColor: "#888",
-    borderWidth: 1,
-  },
-  Icon: {
-    height: 100,
-    width: 200,
-    paddingRight: 150,
-    paddingLeft: 150,
-    // paddingVertical: 30,
-    // paddingBottom: 50,
-    marginLeft: 25,
-    // marginTop: 25,
-  },
-  drop: {
-    borderRadius: 20,
-    backgroundColor: "#EA9547",
-  },
-  infoText: {
-    color: "#EA9547",
-  },
-});
 
 // const { width, height } = Dimensions.get("window");
 // const ASPECT_RATIO = width / height;
